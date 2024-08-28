c85621e80c1f7b9758632ca13d574a53bf61eea9
Szymon Liszewski <szymonlis21@gmail.com>
2024-08-28 21:31:52 +0200

    add tooltips

--- frontend/src/components/TechnicalAnalysis.jsx
+++ frontend/src/components/TechnicalAnalysis.jsx
@@ -2,6 +2,8 @@ import "./styles/TechnicalAnalysis.css"
 import DividendChart from "./DividendChart"
 import RevenueChart from "./RevenueChart"
 import React, {useState, useEffect} from "react";
+import { FaQuestionCircle } from 'react-icons/fa';
+import {Tooltip } from 'react-tooltip';
 
 function formatNumberWithSuffix(number) {
     const suffixes = ["", "K", "M", "B", "T"]; 
@@ -46,29 +48,46 @@ function TechnicalAnalysis({ticker}){
     return(
             <div className="TechnicalAnalysisContainer">
                 <div className="TechnicalContainer" id="Volume"> 
-                    <h3>Volume</h3>
+                    <h3>Volume
+                     <FaQuestionCircle className="questionCircle" data-tooltip-id="marketCap" data-tooltip-content=" The total number of shares traded during a given period. High volume often indicates strong investor interest and can confirm the strength of a price move, while low volume might suggest a lack of conviction or weak price movements."/>
+                     <Tooltip id="marketCap" className="custom-tooltip"/>
+                    </h3>
                     <h1>{formatNumberWithSuffix(SMA200)}</h1>
                 </div>
                 <div className="TechnicalContainer" id="Boelinger"> 
-                     <h3>Boelinger Bands</h3>
+                     <h3>Boelinger Bands
+                     <FaQuestionCircle className="questionCircle" data-tooltip-id="marketCap" data-tooltip-content="Upper Band: Represents a level of high volatility. When the price approaches or exceeds this band, it may indicate an overbought condition.
+ Lower Band: Represents a level of low volatility. When the price approaches or drops below this band, it may suggest an oversold condition."/>
+                     <Tooltip id="marketCap" className="custom-tooltip"/>
+                     </h3>
                      <div className="IndicatorInline">
                         <h1>{Bollinger_High.toFixed(2)}</h1>
                         <h1>{Bollinger_Low.toFixed(2)}</h1>
                      </div>
                 </div>
                 <div className="TechnicalContainer" id="ma"> 
-                    <h3>moving average</h3>
+                    <h3>moving average
+                    <FaQuestionCircle className="questionCircle" data-tooltip-id="marketCap" data-tooltip-content="Short-Term Moving Average: Calculates the average price over a short period. It helps identify recent trends and short-term momentum.
+Long-Term Moving Average: Calculates the average price over a longer period. It smooths out long-term trends and provides insights into the overall direction of the market."/>
+                    <Tooltip id="marketCap" className="custom-tooltip"/>
+                    </h3>
                     <div className="IndicatorInline">
                         <h1>{SMA50.toFixed(2)}</h1>
                         <h1>{SMA200.toFixed(2)}</h1>
                     </div>
                 </div>
                 <div className="TechnicalContainer" id="macd">
-                    <h3>MACD</h3>
+                    <h3>MACD
+                    <FaQuestionCircle className="questionCircle" data-tooltip-id="marketCap" data-tooltip-content="A high value indicates strong bullish momentum (consider buying or holding), while a low value signals strong bearish momentum (consider selling or avoiding)."/>
+                    <Tooltip id="marketCap" className="custom-tooltip"/>
+                    </h3>
                     <h1>{SMA50}</h1> 
                 </div>
                 <div className="TechnicalContainer" id="RSI">
-                    <h3>RSi</h3>
+                    <h3>RSi
+                    <FaQuestionCircle className="questionCircle" data-tooltip-id="marketCap" data-tooltip-content="Measures the speed and change of price movements on a scale from 0 to 100. An RSI above 70 can indicate an overbought condition, while an RSI below 30 might suggest an oversold condition."/>
+                    <Tooltip id="marketCap" className="custom-tooltip"/>
+                    </h3>
                     <h1>{RSI.toFixed(2)}</h1> 
                 </div>
                 
