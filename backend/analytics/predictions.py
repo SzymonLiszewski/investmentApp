24f1b663fc467f57082c9dfd55a1683c582b785c
Szymon <szymonlis21@gmail.com>
2024-12-30 12:39:59 +0100

    added prediction models

--- backend/analytics/predictions.py
+++ backend/analytics/predictions.py
@@ -5,6 +5,10 @@ from sklearn.linear_model import LinearRegression
 import yfinance as yf
 from datetime import timedelta
 from datetime import datetime
+import pickle
+from tensorflow import keras
+import os
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 
 
 def linear_regression_predict(ticker, start_date, end_date, predicted_days=30):
@@ -45,6 +49,63 @@ def linear_regression_predict(ticker, start_date, end_date, predicted_days=30):
     for i in range(len(future_dates)):
         data_dict[future_dates[i]] = future_predictions[i][0]
     return {list(data_dict.keys())[-1]:list(data_dict.values())[-1]}
+
+def load_lstm_model(ticker, start_date, end_date, predicted_days=30):
+    data = yf.download(ticker, start=start_date, end=end_date)
+    close_prices = data['Close']
+
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    scaler_path = os.path.join(BASE_DIR, 'analytics','saved_models','GSPC', 'scaler.pkl')
+    with open(scaler_path, 'rb') as scaler_file:
+        scaler = pickle.load(scaler_file)
+
+
+    scaled_data = scaler.transform(close_prices.values.reshape(-1, 1))
+    model_path = os.path.join(BASE_DIR, 'analytics','saved_models','GSPC', 'my_model.h5')
+    model = keras.models.load_model(model_path)
+
+    # Przewidywanie na przyszłość
+    look_back = 100
+    future_days = predicted_days  
+    last_sequence = scaled_data[-look_back:]
+    predictions = []
+
+    for _ in range(future_days):
+        pred = model.predict(last_sequence.reshape(1, look_back, 1))
+        predictions.append(pred[0, 0])
+        last_sequence = np.append(last_sequence[1:], pred, axis=0)
+
+    # Denormalizacja przewidywanych danych
+    predictions = scaler.inverse_transform(np.array(predictions).reshape(-1, 1))
+
+    # Tworzenie DataFrame dla przewidywanych danych
+    future_dates = pd.date_range(start=close_prices.index[-1] + pd.Timedelta(days=1), periods=future_days)
+    predicted_df = pd.DataFrame(predictions, columns=['Predicted_Close'], index=future_dates)
+    return predicted_df
+
+def sarima(ticker, start_date, end_date, predicted_days=30):
+    data = yf.download(ticker, start=start_date, end=end_date)
+    close_prices = data['Close']
+
+    #* Preparing SARIMA model
+    model = SARIMAX(close_prices, order=(1, 1, 1), seasonal_order=(1, 1, 1, 12))
+    model_fit = model.fit(disp=False)
+
+    #* Prediction
+    future_steps = predicted_days
+    future_predictions = model_fit.get_forecast(steps=future_steps)
+    forecast_ci = future_predictions.conf_int()
+    forecast_values = future_predictions.predicted_mean
+
+    format_string = "%Y-%m-%d"
+    last_date = datetime.strptime(end_date, format_string)
+    future_dates = [(last_date + timedelta(days=i)).strftime(format_string) for i in range(1, predicted_days + 1)]
+
+    # Tworzenie słownika dat i prognozowanych cen
+    data_dict = {date: price for date, price in zip(future_dates[-2:-1], forecast_values[-2:-1])}
+    print(data_dict)
+
+    return data_dict
 
 
 #* prepare time series for regression
