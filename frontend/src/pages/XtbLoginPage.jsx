c8433cf545e9e405c1e4e56021433516ce765980
Szymon <szymonlis21@gmail.com>
2025-01-03 17:17:27 +0100

    add info not in login page

--- frontend/src/pages/XtbLoginPage.jsx
+++ frontend/src/pages/XtbLoginPage.jsx
@@ -3,6 +3,7 @@ import XtbLogin from "../components/login/XtbLogin";
 function XtbLoginPage(){
     return (
         <div>
+          <p> Note: Currently, this feature only supports XTB demo accounts. The functionality is under development, so errors or incomplete features may occur.</p>
           <XtbLogin />
         </div>
       );
